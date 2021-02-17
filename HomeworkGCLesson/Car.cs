f3407804585e0786c6e2048dd3fb7c532ce494c4
Anuar <beisekeyev.a.1992.ast@gmail.com>
2021-02-17 17:06:33 +0600

    Added homework for the lesson GC

---
+++ HomeworkGCLesson/Car.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeworkGCLesson
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ HomeworkGCLesson/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace HomeworkGCLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. С помощью рефлексии получить список методов класса Console и вывести на экран.
+            // 2.Описать класс с несколькими свойствами.Создать экземпляр класса и инициализировать его свойства.
+            // С помощью рефлексии получить свойства и их значения из созданного экземпляра класса. Вывести полученные значения на экран
+
+            Type type = typeof(Console);
+            foreach(var item in type.Assembly.GetTypes())
+            {
+                if(type.Name == "Console" && type.IsClass)
+                {
+                    foreach(var method in type.GetMethods())
+                    {
+                        if(method is MethodInfo)
+                        {
+                            Console.WriteLine(method);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("-------------------------------------------------");
+
+            Car car = new Car()
+            {
+                Id = 1,
+                Name = "BMW",
+                Model = "X5",
+                Price = 15000000
+
+            };
+            Type typeCar = typeof(Car);
+            Console.WriteLine("Список свойств класса Car и его занчения:");
+            PropertyInfo[] propertyInfos = typeCar.GetProperties();
+            foreach(var item in propertyInfos)
+            {
+                if(item.GetIndexParameters().Length == 0)
+                {
+                    Console.WriteLine($"{item.Name} {item.PropertyType} = {item.GetValue(car,null)}");
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
